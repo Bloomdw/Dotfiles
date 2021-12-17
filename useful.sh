3a3126c8d3e85d7ec4cf2256ec08914d78374766
julk <julkyways@gmail.com>
2021-12-17 00:34:03 -0500

    Pog

--- useful.sh
+++ useful.sh
@@ -6,3 +6,12 @@ xdg-mime default libreoffice.desktop application/pdf
 
 #Check which display server you're using
 loginctl show-session $(awk '/tty/ {print $1}' <(loginctl)) -p Type | awk -F= '{print $2}'
+
+#Useful for disks mounting disks etc etc.
+blkid
+
+#Screenshot an area
+flameshot gui -d 2000 -p ~/Images/Screenshots/&
+
+#Self-explanatory. Key mapping
+xmodmap
